6f48dd248fe5868f6f3770a4e614a1b84125b2ee
Arham Sayyed <arhamsayyed56@gmail.com>
2023-08-28 20:10:46 +0530

    added sys.args functionality

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-def main():
+import sys, os
+
+args = sys.argv
+
+def main(args_list):
     try:
-        
+        if not len(args_list) == 3:
+            print("invalid arguments!")
+            exit()
+
+        if (args_list[1] == "-u") and os.path.exists(args_list[2]):
+            upload(file_path = args_list[2])
+
+        elif args_list[1] == "-g":
+            download(url = args_list[2])
+
         return True
     except Exception as e:
         print(f"Error: {e} \nin main")
         return False
+    
+
+
