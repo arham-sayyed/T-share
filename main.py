83ad33923d01469ec55ef55d0827c0f6daa55bef
Arham Sayyed <arhamsayyed56@gmail.com>
2023-08-28 23:52:47 +0530

    added env functionalities

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys, os
+from connecs.envs import check_env_token, update_token
 
 args = sys.argv
 
@@ -9,12 +10,17 @@ def main(args_list):
             exit()
 
         if (args_list[1] == "-u") and os.path.exists(args_list[2]):
+            if not check_env_token():
+                print("auth token is absent! \nuse `--change-token <token>` to set token")
             upload(file_path = args_list[2])
 
         elif args_list[1] == "-g":
             download(url = args_list[2])
 
-        return True
+        elif args_list[1] == "--change-token":
+            update_token(args_list[2])
+
+            
     except Exception as e:
         print(f"Error: {e} \nin main")
         return False
