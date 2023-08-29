4f1079dbc67a9d34a3e3fbff828b7763fc70a5f7
Arham Sayyed <arhamsayyed56@gmail.com>
2023-08-29 12:08:16 +0530

    added upload func

---
+++ connecs/reqs.py
@@ -0,0 +1,30 @@
+import requests
+from connecs.envs import get_token
+
+
+API_ENDPOINT = "https://api.web3.storage"
+
+
+
+def upload_file(file_path):
+
+    token = get_token()
+
+    with open(file_path, 'rb') as f:
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/x-directory"
+        }
+
+        response = requests.post(f"{API_ENDPOINT}/upload", headers=headers, data=f)
+
+    if response.status_code == 200:
+        print(response.json())
+        print("\n\n")
+        cid = response.json()["cid"]
+        return f"https://dweb.link/ipfs/{cid}"
+
+    else:
+        print("Upload Error:", response.text)
+        return None
+
