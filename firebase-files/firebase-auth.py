c10ed3622cafae1ad156b534441fe9117583dc45
Arham Sayyed <arhamsayyed56@gmail.com>
2023-08-30 23:20:29 +0530

    combined for cleaner code

---
+++ firebase-files/firebase-auth.py
@@ -0,0 +1,43 @@
+import pyrebase, json, os
+
+
+CONFIG_FILE = "config.json"
+
+def load_config_from_file():
+    """Load the token from the config file, if it exists."""
+    if os.path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE, 'r') as f:
+            config = json.load(f)
+            return config.get("config", None)
+    return None
+
+config = load_config_from_file()
+
+if config:
+    firebase = pyrebase.initialize_app(config)
+    auth = firebase.auth()
+
+def register_user(email, password) -> dict | None:
+    try:
+        user = auth.create_user_with_email_and_password(email, password)
+        return user
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+def login_user(email, password):
+    try:
+        user = auth.sign_in_with_email_and_password(email, password)
+        return user
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+def verify_user(token):
+    try:
+        user = auth.get_account_info(token)
+        return user
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
